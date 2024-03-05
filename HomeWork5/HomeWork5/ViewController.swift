4b9240991f2a925e9b50a449c09cd927c520f935
Oleksandr-Matsenko <olexandr282@gmail.com>
2024-03-05 18:33:19 +0200

    Add empty project

---
+++ HomeWork5/HomeWork5/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  HomeWork5
+//
+//  Created by Alex Matsenko on 05.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
